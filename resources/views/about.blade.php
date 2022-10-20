0de6d4762ea377359cdc1e567c932cadbac3924a
Rahman 203040132 <rahmanr0918@gmail.com>
2022-10-20 22:39:46 +0700

    Vid.2 - Struktur folder, routes dan View

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Prak Web | About</title>
+</head>
+<body>
+  <h1>Halaman About</h1>
+  
+  <h2><?= $nama; ?></h2>
+  <h5><?= $email; ?></h5>
+</body>
+</html>
