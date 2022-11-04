a00e876531fe31a23929e59717cf596e20cd6b1e
Rahman 203040132 <rahmanr0918@gmail.com>
2022-11-04 23:10:17 +0700

    Vid.10 - Factory dan Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,21 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // User::create([
+        //     'name' => 'Rahman Ramadan',
+        //     'email' => 'rahman@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        User::create([
-            'name' => 'Rahman Ramadan',
-            'email' => 'rahman@gmail.com',
-            'password' => bcrypt('12345')
+        // ]);
 
-        ]);
+        // User::create([
+        //     'name' => 'Galih',
+        //     'email' => 'Galih@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        User::create([
-            'name' => 'Galih',
-            'email' => 'Galih@gmail.com',
-            'password' => bcrypt('12345')
+        // ]);
 
-        ]);
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'web-Programming',
@@ -43,7 +43,9 @@ class DatabaseSeeder extends Seeder
             'slug' => 'Personal'
         ]);
 
-        Post::create([
+        Post::factory(10)->create();
+
+        /*    Post::create([
             'title' => 'Judul Pertama',
             'slug' => 'judul-Pertama',
             'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem!',
@@ -68,6 +70,8 @@ class DatabaseSeeder extends Seeder
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem! Adipisci facere laboriosam excepturi nemo fugiat quae neque necessitatibus asperiores, commodi eveniet error alias officia est. Fugiat numquam delectus corporis autem ut atque nisi excepturi, commodi beatae earum soluta eos doloribus illo sequi? Ab nobis ratione labore accusamus? Natus culpa quia maiores! Molestias iste eum error corrupti, nam voluptates neque facilis pariatur. Dolor nostrum, quisquam nemo libero quibusdam numquam. Modi, corporis libero maiores culpa possimus impedit? Sequi, ratione? Dolore nisi pariatur quos aperiam minus, natus voluptas dolorum! Mollitia aspernatur saepe nobis asperiores aut.',
             'category_id' => 2,
             'user_id' => 2
-        ]);
+        ]); 
+        
+    */
     }
 }
