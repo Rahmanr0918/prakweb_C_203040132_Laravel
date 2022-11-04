a4ab180c9df9bde73405796d765a6e530252e706
Rahman 203040132 <rahmanr0918@gmail.com>
2022-11-04 22:11:05 +0700

    Vid.9 - Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,56 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Rahman Ramadan',
+            'email' => 'rahman@gmail.com',
+            'password' => bcrypt('12345')
+
+        ]);
+
+        User::create([
+            'name' => 'Galih',
+            'email' => 'Galih@gmail.com',
+            'password' => bcrypt('12345')
+
+        ]);
+
+        Category::create([
+            'name' => 'web-Programming',
+            'slug' => 'web-Programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'Personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-Pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem! Adipisci facere laboriosam excepturi nemo fugiat quae neque necessitatibus asperiores, commodi eveniet error alias officia est. Fugiat numquam delectus corporis autem ut atque nisi excepturi, commodi beatae earum soluta eos doloribus illo sequi? Ab nobis ratione labore accusamus? Natus culpa quia maiores! Molestias iste eum error corrupti, nam voluptates neque facilis pariatur. Dolor nostrum, quisquam nemo libero quibusdam numquam. Modi, corporis libero maiores culpa possimus impedit? Sequi, ratione? Dolore nisi pariatur quos aperiam minus, natus voluptas dolorum! Mollitia aspernatur saepe nobis asperiores aut.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-Kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem! Adipisci facere laboriosam excepturi nemo fugiat quae neque necessitatibus asperiores, commodi eveniet error alias officia est. Fugiat numquam delectus corporis autem ut atque nisi excepturi, commodi beatae earum soluta eos doloribus illo sequi? Ab nobis ratione labore accusamus? Natus culpa quia maiores! Molestias iste eum error corrupti, nam voluptates neque facilis pariatur. Dolor nostrum, quisquam nemo libero quibusdam numquam. Modi, corporis libero maiores culpa possimus impedit? Sequi, ratione? Dolore nisi pariatur quos aperiam minus, natus voluptas dolorum! Mollitia aspernatur saepe nobis asperiores aut.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-Ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem!',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iste veritatis saepe similique sit itaque praesentium exercitationem! Adipisci facere laboriosam excepturi nemo fugiat quae neque necessitatibus asperiores, commodi eveniet error alias officia est. Fugiat numquam delectus corporis autem ut atque nisi excepturi, commodi beatae earum soluta eos doloribus illo sequi? Ab nobis ratione labore accusamus? Natus culpa quia maiores! Molestias iste eum error corrupti, nam voluptates neque facilis pariatur. Dolor nostrum, quisquam nemo libero quibusdam numquam. Modi, corporis libero maiores culpa possimus impedit? Sequi, ratione? Dolore nisi pariatur quos aperiam minus, natus voluptas dolorum! Mollitia aspernatur saepe nobis asperiores aut.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 <article>
   <h2>{{ $post->title }}</h2>
-  <p>By. Rahman R9 in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
   {{ $post->body }}
 </article>
     
